68715c830c38ac746e123cecfbfc09ede2a7478f
hoge hoge <hogehoge@gmail.com>
2024-01-28 23:43:47 +0900

    Posrgresqlを動かした
    postgresqlの設定を行って、sequilizeからデータテーブルとモデルを作成した状態。データ未格納、検索コマンドでエラーが出ており、正常稼働しない状態

--- copilotTest/routes/login.js
+++ copilotTest/routes/login.js
@@ -1,21 +1,42 @@
 var express = require("express");
 var router = express.Router();
+const User = require("../models/user");
 
 router.post("/", function (req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
 
   // ログインが成功したらユーザーを次のページにリダイレクト
-  if (username.length > 0 && password.length > 0) {
-    if (password === "pass") {
-      //res.send("to nextpage");
-      res.render("profile", {title: 'profilePage'});
-    } else {
-      res.send("Invalid username or password");
-    }
-  } else {
-    res.send("Invalid username or password");
-  }
+  //   if (username.length > 0 && password.length > 0) {
+  //     if (password === "pass") {
+  //       //res.send("to nextpage");
+  //       res.render("profile", {title: 'profilePage'});
+  //     } else {
+  //       res.send("Invalid username or password");
+  //     }
+  //   } else {
+  //     res.send("Invalid username or password");
+  //   }
+  // });
+  User.findOne({ where: { username: username, password: password } })
+    .then((user) => {
+      if (user) {
+        if (password == user.password) {
+          // ログイン成功
+          res.render("profile", { title: "profilePage" });
+        } else {
+          // ログイン失敗（パスワード不一致）
+          res.send("Invalid username or password");
+        }
+      } else {
+        // ログイン失敗（ユーザーが存在しない）
+        res.send("Invalid username or password");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send("Error occurred while processing your request");
+    });
 });
 
 module.exports = router;
