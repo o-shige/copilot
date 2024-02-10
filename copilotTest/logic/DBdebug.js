12f482897fc9849842c815e3cb2837913d7ead77
hoge hoge <hogehoge@gmail.com>
2024-02-10 18:21:20 +0900

    DBと通信して取得した情報をもとにユーザー情報を評価
    DBとのアクセスロジック完了

--- copilotTest/logic/DBdebug.js
+++ copilotTest/logic/DBdebug.js
@@ -1,9 +1,28 @@
-const {User} = require('../models/index');
+const { User } = require("../models/index");
 
 async function getUsers() {
-  const users = await User.findAll().then((users) => {
-    console.log(users);
-  });
+  // const users = await User.findAll().then((users) => {
+  //   console.log(users);
+  // });
+  try {
+    const users = await User.findAll();
+    //console.log(users);
+    return users;
+  } catch (error) {
+    console.error("Error fetching users: ", error);
+    throw error;
+  }
+}
+
+async function getUser(username) {
+  try {
+    const user = await User.findOne({ where: { username: username } });
+    //console.log(user);
+    return user;
+  } catch (error) {
+    console.error("Error fetching users: ", error);
+    throw error;
+  }
 }
 
 async function createUser(username, password) {
@@ -18,4 +37,4 @@ async function createUser(username, password) {
 // createUser("user1", "pass1");
 // getUsers();
 
-module.exports = { createUser, getUsers };  
+module.exports = { createUser, getUsers, getUser };
--- copilotTest/routes/login.js
+++ copilotTest/routes/login.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../models/index");
-const { createUser, getUsers } = require("../logic/DBdebug");
-
-
+const { createUser, getUsers, getUser } = require("../logic/DBdebug");
 
 router.get("/", function (req, res) {
   //res.render("index", {title: 'home', message: 'hello'});
@@ -11,47 +9,34 @@ router.get("/", function (req, res) {
   //res.send("hello");
 });
 
-router.post("/", function (req, res, next) {
+router.post("/", async function (req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
 
-  // ログインが成功したらユーザーを次のページにリダイレクト
-  //   if (username.length > 0 && password.length > 0) {
-  //     if (password === "pass") {
-  //       //res.send("to nextpage");
-  //       res.render("profile", {title: 'profilePage'});
-  //     } else {
-  //       res.send("Invalid username or password");
-  //     }
-  //   } else {
-  //     res.send("Invalid username or password");
-  //   }
-  // });
+  // ログインが成功したらユーザーを次のページにリダイレクト;
+  if (username.length > 0 && password.length > 0) {
+    // DBから、ユーザー名とパスワードが一致するユーザーを取得
+    // const users = getUsers();
+    // (user) => console.log(user);
+    let user;
+    try {
+      user = await getUser(username);
+      //console.log(user.password);
+    } catch (error) {
+      console.error("Error fetching users: ", error);
+      throw error;
+    }
 
-  //createUser("user1", "pass1");
-  getUsers();
-  res.send("to profile");
-  
-
-  //   User.findOne({ where: { username: username, password: password } })
-  //     .then((user) => {
-  //       if (user) {
-  //         if (password == user.password) {
-  //           // ログイン成功
-  //           res.render("profile", { title: "profilePage" });
-  //         } else {
-  //           // ログイン失敗（パスワード不一致）
-  //           res.send("Invalid username or password");
-  //         }
-  //       } else {
-  //         // ログイン失敗（ユーザーが存在しない）
-  //         res.send("Invalid username or password");
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //       res.send("Error occurred while processing your request");
-  //     });
+    // パスワード評価
+    if (password === user.password) {
+      //res.send("to nextpage");
+      res.render("profile", {title: 'profilePage'});
+    } else {
+      res.send("Invalid username or password");
+    }
+  } else {
+    res.send("Invalid username or password");
+  }
 });
 
 module.exports = router;
